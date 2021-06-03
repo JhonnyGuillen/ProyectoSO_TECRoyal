ac3f7b65f558a4ca441a5490d9ee2441ecd6f071
Jean Marco Rojas <jeanmarco1997@gmail.com>
2021-06-02 21:27:38 -0600

    timer

---
+++ Timer.h
@@ -0,0 +1,17 @@
+#ifndef TIMER_H
+#define	TIMER_H
+
+#define INTERVAL 5000; // 5ms - quantum //
+#define START 10; // 1 ms //
+#include <signal.h>
+#include <sys/time.h>
+struct itimerval it;
+sigset_t t1, t2;
+
+void lockSignals();
+void unlockSignals();
+void pauseTimer();
+void startTimer();
+void setupTimer(struct sigaction act);
+
+#endif	/* TIMER_H */
